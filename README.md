bff6fe6153996c3c1054f46b1cb1c423f24091b9
notlegos <roger@mustacich.com>
2024-06-23 11:51:23 -0700

    Update main.blocks, main.ts, README.md

--- README.md
+++ README.md
@@ -22,3 +22,15 @@ To edit this repository in MakeCode.
 
 * for PXT/microbit
 <script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
+
+
+In Folder 02 of the MP3Bit:
+<br>cannon.mp3
+<br>die.mp3
+<br>hurry.mp3
+<br>mario.mp3
+<br>over.mp3
+<br>star.mp3
+<br>thwomp.mp3
+<br>victory.mp3
+<br>win.mp3
--- main.ts
+++ main.ts
@@ -1,10 +1,30 @@
-makerbit.onIrButton(IrButton.Any, IrButtonAction.Pressed, function () {
-    basic.showIcon(IconNames.Heart)
-    basic.pause(500)
-    basic.clearScreen()
-})
-makerbit.connectIrReceiver(DigitalPin.P1, IrProtocol.NEC)
-basic.showIcon(IconNames.Surprised)
+let hits = 0
+let lastHitWasRed = false
+let isRed = false
+basic.showIcon(IconNames.Confused)
+let strip = Connected.create(Connected.DigitalRJPin.J1, 8, Connected.NeoPixelMode.RGB)
+let hitsRequired = 10
+strip.showRainbow(1, 360)
+Connected.MP3SetPort(Connected.DigitalRJPin.J2)
+basic.pause(1000)
+basic.showIcon(IconNames.SmallHeart)
 basic.forever(function () {
-	
+    strip.showColor(Connected.colors(Connected.NeoPixelColors.Red))
+    isRed = Connected.checkColor(Connected.ColorList.red)
+    if (isRed && !(lastHitWasRed)) {
+        hits = 1
+    } else if (isRed && lastHitWasRed) {
+        hits = hits + 1
+    } else {
+        if (hits == hitsRequired) {
+            basic.showIcon(IconNames.Heart)
+            Connected.setVolume(6)
+            Connected.folderPlay("01", "001")
+            basic.pause(5000)
+            Connected.execute(Connected.playType.Stop)
+        }
+        hits = 0
+    }
+    lastHitWasRed = isRed
+    basic.clearScreen()
 })
